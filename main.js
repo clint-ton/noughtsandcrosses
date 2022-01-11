6ee45e77bf35f0964d85904e22f3bd528100f762
clinton <cjw237@uclive.ac.nz>
2022-01-11 13:04:16 +1300

    added ability to make moves

--- main.js
+++ main.js
@@ -1,39 +1,113 @@
-const gridSize = 3;
+const GRID_SIZE = 3;
 
 const gameBoard = (() => {
-    let board = [
-        [["X"], ["0"], ["X"]],
-        [["0"], ["X"], ["0"]],
-        [["X"], ["0"], ["X"]],
-    ];
-    // for (let i = 0; i < gridSize; i++) {
-    //     board.push([])
-    //     for (let j = 0; j < gridSize; j++) {
-    //         board[i].push(" ");
-    //     }
-    // };
 
+    let _board;
+
+    // clears the board
+    const initBoard = () => { 
+        _board = [
+            [undefined, undefined, undefined],
+            [undefined, undefined, undefined],
+            [undefined, undefined, undefined]
+        ];
+    };
+
+    initBoard();
+
+    const setSquare = (row, col, mark) => {
+        _board[row][col] = mark;
+    };
+
+    function getBoard() {
+        return _board;
+    }
+
+    return { setSquare, getBoard };
 
 })();
 
+function playerFactory(mark)  {
+    let _mark = mark;
+
+    const getMark = () => _mark
+
+    const makeMove = (square) => {
+        square.innerText = mark;
+        game.toggleCurrentPlayer();
+        gameBoard.setSquare(parseInt(square.dataset.row), parseInt(square.dataset.column), mark);
+        console.log(gameBoard.getBoard());
+    }
+
+    return { getMark, makeMove }
+
+}
+
+
+const game = (() => {
+
+    let _p1 = playerFactory("X");
+    let _p2 = playerFactory("0");
+
+
+    let _currentPlayer = _p1;
+
+    const getCurrentPlayer = () => {
+        return _currentPlayer;
+    };
+
+    const toggleCurrentPlayer = () => {
+        if (_currentPlayer == _p1) {
+            _currentPlayer = _p2;
+        } else {
+            _currentPlayer = _p1;
+        };
+        console.log(_currentPlayer)
+    }
+
+    // returns true if a player has won, potentially take in a mark param?
+    const checkWin = () => {
+
+    }
+
+    // returns True if the board is full ie. there is a draw
+    const checkDraw = () => {
+        for (let i = 0; i < GRID_SIZE; i++) {
+            for (let j = 0; j < GRID_SIZE; j++) {
+                if (_board === undefined) {
+                    return false;
+                };
+            };
+        };
+        return true;
+    }
+
+    return { getCurrentPlayer, toggleCurrentPlayer }
+    
+})();
+
 const displayController = (() => {
-    let drawBoard = () => {
+    const drawBoard = () => {
         const board = document.querySelector("#board");
-        for (let i = 0; i < gridSize; i++) {
+        for (let i = 0; i < GRID_SIZE; i++) {
             const row = document.createElement("div");
             row.classList.add("row");
-            for (let j = 0; j < gridSize; j++) {
+            for (let j = 0; j < GRID_SIZE; j++) {
                 const square = document.createElement("div");
                 square.classList.add("square");
+                square.dataset.row = i;
+                square.dataset.column = j
+                square.addEventListener('click', () => {
+                    game.getCurrentPlayer().makeMove(square);
+                })
                 row.appendChild(square);
             };
             board.appendChild(row);
         };
     };
 
-
-
     return { drawBoard };
 })();
 
 displayController.drawBoard();
+
